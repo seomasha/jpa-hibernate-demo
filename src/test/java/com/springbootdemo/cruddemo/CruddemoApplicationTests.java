f184912ef9e843eb594ef9556e8088fa00b66646
seadmasetic <75685855+seomasha@users.noreply.github.com>
2024-09-13 11:21:00 +0200

    Initial commit

---
+++ src/test/java/com/springbootdemo/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootdemo.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
